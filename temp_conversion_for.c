a918cdc8e1d3044d3c275a49fcc4e24dd6dda185
ridged_chip <ridged_chip@protonmail.com>
2024-08-26 00:13:22 -0700

    Added a temperature conversion program. This one uses a for loop instead of a while for its iterating.

---
+++ temp_conversion_for.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* Prints the Fahrenheit-Celsius table, just like temp_conversion_while.c
+ * except using a slightly different syntax. */
+int main()
+{
+				int f;
+
+				for (f = 0; f <= 300; f = f + 20)
+								printf("%3d %6.1f\n", f, (5.0/9.0)*(f-32));
+}
