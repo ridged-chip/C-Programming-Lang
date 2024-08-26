a8e29cf625f0c700259658a553ea855a08eb9b14
ridged_chip <ridged_chip@protonmail.com>
2024-08-26 10:44:34 -0700

    Updated the echo program to use a more concise while loop instead.

--- CHPT1/file_copy.c
+++ CHPT1/file_copy.c
@@ -5,10 +5,13 @@ int main() {
 				 * EOF characters */
 				int c;
 
-				c = getchar();
-				while (c != EOF) {
+				/* Note here too c = getchar() is surrounded by parenthesis.
+				 * This is necessary, because the precedence of an inequality operator (!=) 
+				 * is higher than that of assignment (=). Without the paranthesis, 
+				 * c would either be assigned 0 or 1 depending on if EOF was hit. */
+
+				while ((c = getchar()) != EOF) {
 								putchar(c);
-								c = getchar();
 				}
 
 				return 0;
