6309462963a70c801b8f418f008b06cb9cae1982
ridged_chip <ridged_chip@protonmail.com>
2024-08-26 00:18:57 -0700

    Changed the temp_conversion_for code so that it now uses pre-compiler constants in place of hardcoded values for the highest/lowest/step values.

--- temp_conversion_for.c
+++ temp_conversion_for.c
@@ -2,10 +2,15 @@
 
 /* Prints the Fahrenheit-Celsius table, just like temp_conversion_while.c
  * except using a slightly different syntax. */
+
+#define LOWER 0
+#define UPPER 300
+#define STEP  20
+
 int main()
 {
 				int f;
 
-				for (f = 300; f >= 0; f = f - 20)
+				for (f = UPPER; f >= LOWER; f = f - STEP)
 								printf("%3d %6.1f\n", f, (5.0/9.0)*(f-32));
 }
